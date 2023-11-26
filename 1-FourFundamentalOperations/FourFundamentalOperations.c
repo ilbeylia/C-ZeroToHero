603e92a9aaa33da5bcf25d2a6ec71ddbd6dcdd16
Abdurrahman ilbeyli <ilbeyli187@gmail.com>
2023-11-26 04:06:14 +0300

    C-ZeroToHero
    Four basic arithmetic operation project has been added.

---
+++ 1-FourFundamentalOperations/FourFundamentalOperations.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int Addition(int value1, int value2) {
+    return value1 + value2;
+}
+
+int Subtraction(int value1, int value2) {
+    return value1 - value2;
+}
+
+int Multiplication(int value1, int value2) {
+    return value1 * value2;
+}
+
+int Division(int value1, int value2) {
+    if (value2 != 0) {
+        return value1 / value2;
+    } else {
+        printf("The divisor cannot be zero.\n");
+        return 0;
+    }
+}
+
+int main() {
+    int value1, value2, result;
+    char operation;
+    
+    printf("Enter the two numbers and the operator for the operation to be performed: ");
+    scanf("%d, %d, %c", &value1, &value2, &operation);
+
+    switch (operation) {
+        case '+':
+            result = Addition(value1, value2);
+            break;
+        case '-':
+            result = Subtraction(value1, value2);
+            break;
+        case '*':
+            result = Multiplication(value1, value2);
+            break;
+        case '/':
+            result = Division(value1, value2);
+            break;
+        default:
+            printf("Invalid operator.\n");
+            return 1; // Hatalı durumda programı sonlandır
+    }
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
---
+++ 1-FourFundamentalOperations/README.md
@@ -0,0 +1,12 @@
+# C-ZeroToHero
+
+## Overview
+
+"C-ZeroToHero" is a repository dedicated to advancing skills in the C programming language. The goal is to build projects ranging from low-level to high-level, exploring various structures and concepts.<br>
+## Projects<br>
+1.FourFundamentalOperations
+2.will be added...
+
+## Contributing<br>
+Feel free to contribute by adding more projects, improving existing code, or suggesting enhancements. Fork the repository, make your changes, and submit a pull request.<br>
+
